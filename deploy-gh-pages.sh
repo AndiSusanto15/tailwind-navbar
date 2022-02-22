efe316d16d63a422d817e07cdf124bf526ec7109
Andi <susanto.andi11@gmail.com>
2022-02-22 21:12:43 +0700

    add directory docs

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-echo 'Build to gh-pages...'
-npx tailwindcss -o ./gh-pages/css/style.css --minify
-mkdir -p gh-pages/css
-cp -R public/* gh-pages
+echo 'Build to directory ./docs...'
+npx tailwindcss -o ./docs/css/style.css --minify
+cp -R public/* docs
 
-echo 'Build succesfull'
+echo 'Build succesfull...'
